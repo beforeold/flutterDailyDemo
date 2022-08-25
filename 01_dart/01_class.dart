e308363d74d979f7a4b58a4e8f37aad9b1dc638e
beforeold <ibeforeold@gmail.com>
2022-08-25 19:19:13 +0800

    demo dart package

--- 01_dart/01_class.dart
+++ 01_dart/01_class.dart
@@ -1,3 +1,18 @@
-void main(List<String> args) {
-  print("hello dart");
+import 'utils/math.dart';
+import 'package:http/http.dart' as http;
+
+void main(List<String> args) async {
+  print(sum(5, 1));
+  print("hello dart 2");
+
+  test_http();
+}
+
+void test_http() async {
+  var url = Uri.https('example.com', 'whatsit/create');
+  var response =
+      await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
+  print(await http.read(Uri.https('example.com', 'foobar.txt')));
 }
