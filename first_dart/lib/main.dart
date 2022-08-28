503f309281a3287688b3afbac1f10cd879f531f3
beforeold <ibeforeold@gmail.com>
2022-08-28 22:17:49 +0800

    test dart extension

--- first_dart/lib/main.dart
+++ first_dart/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 // void main() {
@@ -13,20 +14,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(title: const Text("first_dart")),
-        body: const MyHomePage(),
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Scaffold(
+          appBar: AppBar(title: const Text("first_dart")),
+          body: const MyHomePage(),
+        ));
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
+  void sayHome() {
+    print("say home");
+  }
 
   @override
   State<StatefulWidget> createState() {
@@ -36,10 +39,40 @@ class MyHomePage extends StatefulWidget {
 
 class MyHomePageState extends State<MyHomePage> {
   bool flag = false;
-  
+
   @override
   Widget build(BuildContext context) {
-    return Text("hello");
+    widget.coolPrint();
+    return const Text("hello")
+        .padding()
+        .backgroundColor()
+        .centered();
+  }
+}
+
+extension MyExtension on Widget {
+  void coolPrint() {
+    if (kDebugMode) {
+      print("cool print $this");
+    }
   }
 
+  Center centered() {
+    return Center(
+      child: this,
+    );
+  }
+
+  Padding padding() {
+    return Padding(padding: const EdgeInsets.all(8), child: this);
+  }
+
+  Container backgroundColor() {
+    return Container(
+      height: 50,
+      padding: EdgeInsets.zero,
+      color: Colors.blue,
+      child: this,
+    );
+  }
 }
